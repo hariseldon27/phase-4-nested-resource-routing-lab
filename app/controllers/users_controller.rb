d7c7b01a7b2922f4eb9594f501965dddfb7425e2
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-03-16 16:45:55 -0700

    commit tree e6e07bf3a102f3e95c3d37a2e2e92c3ceba7429c

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,23 @@
 class UsersController < ApplicationController
 
+  def index
+    users = User.all
+    render json: users
+  end
+
   def show
-    user = User.find_by(id: params[:id])
+    user = User.find(params[:id])
     render json: user, include: :items
+  end
+
+  def create
+    user = User.create!(user_params)
+    render json: user, status: :created
+  end
+
+  private
+  def user_params
+    params.permit(:name, :description, :price)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :items, only: [:index]
-  resources :users, only: [:show]
+  resources :items, only: [:index] do
+    resources :users, only: [:index, :show, :create]
+  end
+  resources :users, only: [:show, :index] do
+    resources :items, only: [:index, :show, :create]
+  end
 end
